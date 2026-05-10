722e408a172edcbe28b1d293b7f032c38c6c6e38
FuYnAloft <207443530+fuynaloft@users.noreply.github.com>
2026-05-11 04:54:44 +0800

    feat: 添加场景相关拓展方法；重构拓展方法

---
+++ ManosabaLinCode/Extensions/ResourcePathExtensions.cs
@@ -0,0 +1,22 @@
+﻿using MegaCrit.Sts2.Core.Models;
+using static ManosabaLin.Utils.ResourcePathHelper;
+
+namespace ManosabaLin.Extensions;
+
+public static class ResourcePathExtensions
+{
+    extension(EncounterModel encounter)
+    {
+        public string BackgroungScenePath
+        {
+            get
+            {
+                var id = encounter.Id.Entry.ToLowerInvariant();
+                return BuildResPath("scenes", "backgrounds", id, id + ".tscn");
+            }
+        }
+
+        public string BackgroundLayersDirectoryPath =>
+            BuildResPath("scenes", "backgrounds", encounter.Id.Entry.ToLowerInvariant(), "layers");
+    }
+}
--- ManosabaLinCode/Extensions/StringExtensions.cs
+++ ManosabaLinCode/Extensions/StringExtensions.cs
@@ -1,94 +1,79 @@
+using static ManosabaLin.Utils.ResourcePathHelper;
+
 namespace ManosabaLin.Extensions;
 
 public static class StringExtensions
 {
-    public static string ImagePath(this string path)
+    extension(string path)
     {
-        return BuildResPath("images", path);
-    }
+        public string ImagePath()
+        {
+            return BuildResPath("images", path);
+        }
 
-    public static string CharacterImgPath(this string path, string character)
-    {
-        return BuildResPath("images", "characters", character, path);
-    }
+        public string CharacterImgPath(string character)
+        {
+            return BuildResPath("images", "characters", character, path);
+        }
 
-    public static string CharacterAudioPath(this string path)
-    {
-        return BuildRelativePath("characters", path);
-    }
+        public string CharacterAudioPath()
+        {
+            return BuildRelativePath("characters", path);
+        }
 
-    public static string CharacterScenePath(this string path, string character)
-    {
-        return BuildResPath("scenes", character, path);
-    }
+        public string CharacterScenePath(string character)
+        {
+            return BuildResPath("scenes", character, path);
+        }
 
-    public static string PotionImagePath(this string path)
-    {
-        return BuildResPath("images", "potions", path);
-    }
+        public string PotionImagePath()
+        {
+            return BuildResPath("images", "potions", path);
+        }
 
-    public static string CardsImagePath(this string path)
-    {
-        return BuildResPath("images", "cards", path);
-    }
+        public string CardsImagePath()
+        {
+            return BuildResPath("images", "cards", path);
+        }
 
-    public static string CardsAudioPath(this string path)
-    {
-        return BuildRelativePath("cards", path);
-    }
+        public string CardsAudioPath()
+        {
+            return BuildRelativePath("cards", path);
+        }
 
-    public static string BgmAudioPath(this string path)
-    {
-        return BuildRelativePath("bgm", path);
-    }
+        public string BgmAudioPath()
+        {
+            return BuildRelativePath("bgm", path);
+        }
 
-    public static string BigCardsImagePath(this string path)
-    {
-        return BuildResPath("images", "cards", "big", path);
-    }
+        public string BigCardsImagePath()
+        {
+            return BuildResPath("images", "cards", "big", path);
+        }
 
-    public static string PowerImagePath(this string path)
-    {
-        return BuildResPath("images", "powers", path);
-    }
+        public string PowerImagePath()
+        {
+            return BuildResPath("images", "powers", path);
+        }
 
-    public static string BigPowerImagePath(this string path)
-    {
-        return BuildResPath("images", "powers", "big", path);
-    }
+        public string BigPowerImagePath()
+        {
+            return BuildResPath("images", "powers", "big", path);
+        }
 
-    public static string RelicImagePath(this string path)
-    {
-        return BuildResPath("images", "relics", path);
-    }
+        public string RelicImagePath()
+        {
+            return BuildResPath("images", "relics", path);
+        }
 
-    public static string BigRelicImagePath(this string path)
-    {
-        return BuildResPath("images", "relics", "big", path);
-    }
+        public string BigRelicImagePath()
+        {
+            return BuildResPath("images", "relics", "big", path);
+        }
 
-    public static string CharacterUiPath(this string path)
-    {
-        return BuildResPath("images", "charui", path);
-    }
-
-    private static string BuildResPath(params string[] segments)
-    {
-        return string.Join(
-            "/",
-            new[] { MainFile.ResPath.TrimEnd('/') }
-                .Concat(segments.Select(NormalizeSegment).Where(static s => !string.IsNullOrEmpty(s))));
-    }
-
-    private static string BuildRelativePath(params string[] segments)
-    {
-        return string.Join(
-            "/",
-            segments.Select(NormalizeSegment).Where(static s => !string.IsNullOrEmpty(s)));
-    }
-
-    private static string NormalizeSegment(string segment)
-    {
-        return segment.Replace('\\', '/').Trim('/');
+        public string CharacterUiPath()
+        {
+            return BuildResPath("images", "charui", path);
+        }
     }
 }
