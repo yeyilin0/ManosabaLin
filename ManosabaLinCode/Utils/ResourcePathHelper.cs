cfc1ba8693f9f3382c724420aec4e1fe9a95e543
FuYnAloft <207443530+fuynaloft@users.noreply.github.com>
2026-05-11 04:54:44 +0800

    feat: 添加场景相关拓展方法；重构拓展方法

---
+++ ManosabaLinCode/Utils/ResourcePathHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace ManosabaLin.Utils;
+
+public static class ResourcePathHelper
+{
+    public static string BuildResPath(params string[] segments)
+    {
+        return string.Join(
+            "/",
+            new[] { MainFile.ResPath.TrimEnd('/') }
+                .Concat(segments.Select(NormalizeSegment).Where(static s => !string.IsNullOrEmpty(s))));
+    }
+
+    public static string BuildRelativePath(params string[] segments)
+    {
+        return string.Join(
+            "/",
+            segments.Select(NormalizeSegment).Where(static s => !string.IsNullOrEmpty(s)));
+    }
+
+    public static string NormalizeSegment(string segment)
+    {
+        return segment.Replace('\\', '/').Trim('/');
+    }
+}
