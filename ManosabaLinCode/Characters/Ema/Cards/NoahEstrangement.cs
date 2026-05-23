1416529f367a080208af4316b8522e7fa72f9c95
FuYnAloft <207443530+fuynaloft@users.noreply.github.com>
2026-05-23 18:49:39 +0800

    fix: 修复蝶落

--- ManosabaLinCode/Characters/Ema/Cards/NoahEstrangement.cs
+++ ManosabaLinCode/Characters/Ema/Cards/NoahEstrangement.cs
@@ -6,31 +6,23 @@ using ManosabaLin.Characters.Hiro.Powers;
 using MegaCrit.Sts2.Core.CardSelection;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Entities.Creatures;
-using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using STS2RitsuLib.Interop.AutoRegistration;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace ManosabaLin.Characters.Ema.Cards;
 
 [RegisterCard(typeof(EmalinCardPool))]
-public sealed class NoahEstrangement : ManosabaCardTemplate
+public sealed class NoahEstrangement() : ManosabaCardTemplate(3, CardType.Skill, CardRarity.Rare, TargetType.Self)
 {
-    public NoahEstrangement() : base(3, CardType.Skill, CardRarity.Rare, TargetType.Self) { }
+    protected override IEnumerable<IHoverTip> AdditionalHoverTips =>
+    [
+        HoverTipFactory.FromPower<BondPower>(), HoverTipFactory.FromPower<CrimsonbutterflyPower>()
+    ];
 
-    protected override IEnumerable<IHoverTip> AdditionalHoverTips
-    {
-        get
-        {
-            yield return HoverTipFactory.FromPower<BondPower>();
-            yield return HoverTipFactory.FromPower<CrimsonbutterflyPower>();
-        }
-    }
 
-    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay, ComponentContext componentContext)
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay,
+        ComponentContext componentContext)
     {
         var owner = Owner;
         var creature = owner.Creature;
@@ -41,23 +33,20 @@ public sealed class NoahEstrangement : ManosabaCardTemplate
         var handCards = PileType.Hand.GetPile(owner).Cards.ToList();
         if (handCards.Count > 0)
         {
-            var replayPrefs = new CardSelectorPrefs(SelectionScreenPrompt, 1, 1);
+            var replayPrefs = new CardSelectorPrefs(SelectionScreenPrompt, 1);
             var replaySelected = await CardSelectCmd.FromHand(
-                choiceContext, owner, replayPrefs, null, this);
+                choiceContext, owner, replayPrefs, null, null!);
             var replayCard = replaySelected.FirstOrDefault();
             if (replayCard != null)
-            {
                 replayCard.BaseReplayCount++;
-                CardCmd.Preview(replayCard);
-            }
         }
 
         var discardHand = PileType.Hand.GetPile(owner).Cards.ToList();
         if (discardHand.Count > 0)
         {
             var discardPrefs = new CardSelectorPrefs(SelectionScreenPrompt, 1, 1);
-            var discardSelected = await CardSelectCmd.FromHandForDiscard(
-                choiceContext, owner, discardPrefs, null, this);
+            var discardSelected = await CardSelectCmd.FromHand(
+                choiceContext, owner, discardPrefs, null, null!);
             var discardCard = discardSelected.FirstOrDefault();
             if (discardCard != null)
             {
