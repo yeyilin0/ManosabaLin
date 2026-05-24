dfbfecd12c9f596a1020c21feb5e5c9f76ecc4b1
FuYnAloft <207443530+fuynaloft@users.noreply.github.com>
2026-05-24 20:13:16 +0800

    feat: EmaBadEndingRewardComponent 支持合并，右键展示所有的卡牌

--- ManosabaLinCode/Characters/Ema/Components/EmaBadEndingRewardComponent.cs
+++ ManosabaLinCode/Characters/Ema/Components/EmaBadEndingRewardComponent.cs
@@ -1,60 +1,106 @@
-using Godot;
+using MegaCrit.Sts2.Core.CardSelection;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Saves.Runs;
 using MinionLib.Component;
 using MinionLib.Component.Core;
+using MinionLib.Component.Interfaces;
+using MinionLib.RightClick;
 
 namespace ManosabaLin.Characters.Emalin.Components;
 
 public sealed partial class EmaBadEndingRewardComponent : CardComponent
 {
-    private IHoverTip? _hovertip;
-    [LocArg] private string? CardTitle { get; set; }
+    [ComponentState] private List<SerializableCard> SavedCards { get; set; }
 
-    [ComponentState] private int UseLeft { get; set; }
+    [ComponentState] private List<int> UseLeft { get; set; }
 
-    [ComponentState]
-    private SerializableCard SavedCard
-    {
-        get;
-        set
-        {
-            field = value;
-            var mutable = CardModel.FromSerializable(SavedCard);
-            _hovertip = new CardHoverTip(mutable);
-            CardTitle = mutable.Title;
-        }
-    }
-
-    public override IEnumerable<IHoverTip> HoverTips => _hovertip == null ? [] : [_hovertip];
+    [LocArg] private int CardCount => SavedCards.Count;
 
     public EmaBadEndingRewardComponent(CardModel card)
     {
-        SavedCard = card.ToSerializable();
+        var serializable = card.ToSerializable();
 
-        UseLeft = card.Rarity switch
+        var use = card.Rarity switch
         {
             CardRarity.Rare => 1,
             CardRarity.Uncommon => 2,
             _ => throw new AbandonedMutexException($"Unexpected rarity {card.Rarity} for card {card.Id}")
         };
+
+        SavedCards = [serializable];
+        UseLeft = [use];
+    }
+
+    public override bool TryMergeWith(ICardComponent incoming, ApplyComponentOptions options,
+        out ICardComponent? merged)
+    {
+        if (incoming is not EmaBadEndingRewardComponent other)
+        {
+            merged = null;
+            return false;
+        }
+
+        SavedCards.AddRange(other.SavedCards);
+        UseLeft.AddRange(other.UseLeft);
+        merged = this;
+        return true;
+    }
+
+    private IEnumerable<CardModel> UseOnce()
+    {
+        var newSavedCard = new List<SerializableCard>();
+        var newUseLeft = new List<int>();
+        var result = new List<CardModel>();
+        for (var i = 0; i < CardCount; i++)
+        {
+            result.Add(CardModel.FromSerializable(SavedCards[i]));
+            if (UseLeft[i] > 1)
+            {
+                newSavedCard.Add(SavedCards[i]);
+                newUseLeft.Add(UseLeft[i] - 1);
+            }
+        }
+
+        SavedCards = newSavedCard;
+        UseLeft = newUseLeft;
+        return result;
     }
 
     public override async Task OnPlayPostfix(PlayerChoiceContext choiceContext, CardPlay cardPlay,
         ComponentContext componentContext)
     {
-        var card = CardModel.FromSerializable(SavedCard);
-        Card!.CombatState!.AddCard(card, Card.Owner);
-        await CardPileCmd.AddGeneratedCardToCombat(card, PileType.Play, Card.Owner, CardPilePosition.Top);
-        await CardCmd.AutoPlay(choiceContext, card, null);
-        if (card.Pile is { IsCombatPile: true })
-            await CardPileCmd.RemoveFromCombat(card);
-        UseLeft--;
-        if(UseLeft <= 0)
+        var cards = UseOnce();
+        foreach (var card in cards)
+        {
+            Card!.CombatState!.AddCard(card, Card.Owner);
+            await CardPileCmd.AddGeneratedCardToCombat(card, PileType.Play, Card.Owner, CardPilePosition.Top);
+            await CardCmd.AutoPlay(choiceContext, card, null);
+            if (card.Pile is { IsCombatPile: true })
+                await CardPileCmd.RemoveFromCombat(card);
+        }
+
+        if (CardCount == 0)
             ComponentsCard!.RefRemoveComponent(this);
+    }
+
+    public override bool CanHandleRightClick(RightClickContext context) => true;
+
+    public override async Task OnRightClick(PlayerChoiceContext choiceContext, RightClickContext clickContext)
+    {
+        if (CardCount == 0) return;
+        var cards = SavedCards
+            .Select(CardModel.FromSerializable)
+            .ToList();
+        var prompt = new LocString("cards", $"{ComponentId}.rightClickPrompt");
+        prompt.Add(nameof(CardCount), CardCount);
+        await CardSelectCmd.FromSimpleGrid(choiceContext, cards, Card!.Owner,
+            new CardSelectorPrefs(prompt, 0)
+            {
+                Cancelable = true
+            });
     }
 }
