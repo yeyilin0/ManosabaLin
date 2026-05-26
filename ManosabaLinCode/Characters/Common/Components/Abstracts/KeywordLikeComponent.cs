7b22c465fb8069292a6f3f3b90b7a00e6f5759b6
FuYnAloft <207443530+fuynaloft@users.noreply.github.com>
2026-05-26 17:26:53 +0800

    fix: 完善 KeywordLikeComponent

--- ManosabaLinCode/Characters/Common/Components/Abstracts/KeywordLikeComponent.cs
+++ ManosabaLinCode/Characters/Common/Components/Abstracts/KeywordLikeComponent.cs
@@ -1,21 +1,64 @@
+using System.Collections.Concurrent;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization;
 using MinionLib.Component;
+using MinionLib.Component.Core;
+using MinionLib.Component.Interfaces;
 
 namespace ManosabaLin.Characters.Common.Components.Abstracts;
 
 public abstract class KeywordLikeComponent : CardComponent
 {
-    public override IEnumerable<IHoverTip> HoverTips => [CreateHoverTip(ComponentId)];
+    public override IEnumerable<IHoverTip> HoverTips => GetHoverTipArray(this);
+
+    public override bool TryMergeWith(ICardComponent incoming, ApplyComponentOptions options, out ICardComponent? merged)
+    {
+        if (incoming.GetType() != GetType())
+        {
+            merged = null;
+            return false;
+        }
+
+        merged = this;
+        return true;
+    }
+
+    public override bool TrySubtractiveMergeWith(ICardComponent incoming, ApplyComponentOptions options, out ICardComponent? merged)
+    {
+        if (incoming.GetType() != GetType())
+        {
+            merged = null;
+            return false;
+        }
+
+        merged = null;
+        return true;
+    }
+
 
     private static HoverTip CreateHoverTip(string componentId)
     {
-        return new HoverTip(new LocString("cards", $"{componentId}.hovertip.title"),
-            new LocString("cards", $"{componentId}.hovertip.description"));
+        return new HoverTip(
+            new LocString("cards", $"{componentId}.hovertip.title"),
+            new LocString("cards", $"{componentId}.hovertip.description")
+        );
     }
 
-    public static HoverTip GetHoverTip<T>(T component) where T : KeywordLikeComponent, new()
+    private static readonly ConcurrentDictionary<Type, IHoverTip[]> HoverTipCache = [];
+
+    private static IHoverTip[] GetHoverTipArray(KeywordLikeComponent instance)
     {
-        return CreateHoverTip(new T().ComponentId);
+        return HoverTipCache.GetOrAdd(
+            instance.GetType(),
+            _ => [CreateHoverTip(instance.ComponentId)]
+        );
+    }
+
+    public static IHoverTip[] GetHoverTip<T>() where T : KeywordLikeComponent, new()
+    {
+        return HoverTipCache.GetOrAdd(
+            typeof(T),
+            _ => [CreateHoverTip(new T().ComponentId)]
+        );
     }
 }
