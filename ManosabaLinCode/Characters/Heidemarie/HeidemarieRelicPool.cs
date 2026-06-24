37c0ccb263b496db2924867853929d17dae00b24
magicalastrogy <magical@fakemail.com>
2026-06-24 23:24:33 +0800

    Add Heidemarie character scaffold

---
+++ ManosabaLinCode/Characters/Heidemarie/HeidemarieRelicPool.cs
@@ -0,0 +1,13 @@
+using Godot;
+using ManosabaLin.Extensions;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ManosabaLin.Characters.Heidemarie;
+
+public class HeidemarieRelicPool : TypeListRelicPoolModel
+{
+    public override string EnergyColorName => "heidemarie";
+    public override Color LabOutlineColor => Heidemarie.Color;
+    public override string BigEnergyIconPath => "charui/big_energy.png".ImagePath();
+    public override string TextEnergyIconPath => "charui/text_energy.png".ImagePath();
+}
